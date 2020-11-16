86b05bc5554a727d5a4404fa9c8f6a654f1e9e3e
emmikoivisto <emetsi@gmail.com>
2020-11-16 17:49:22 +0000

    waterbottle tests & functions added

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,5 +1,5 @@
 public class WaterBottle {
-    private int volume;
+    public int volume;
 
     public WaterBottle(int volume) {
         this.volume = 100;
@@ -12,5 +12,17 @@ public class WaterBottle {
     public int removeDrink(int newDrink) {
         this.volume -= newDrink;
         return this.volume;
+    }
+
+    public int clearVolume() {
+        return this.volume = 0;
+    }
+
+    public int fillVolume() {
+        if (this.volume <= 100) {
+            return this.volume = 100;
+        } else {
+            return this.volume;
+        }
     }
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -21,4 +21,17 @@ public class WaterBottleTest {
         waterBottle.removeDrink(10);
         assertEquals(90, waterBottle.getVolume());
     }
+
+    @Test
+    public void canClearVolume() {
+        waterBottle.clearVolume();
+        assertEquals(0, waterBottle.volume);
+    }
+
+    @Test
+    public void canFillVolume() {
+        WaterBottle waterBottle = new WaterBottle(10);
+        waterBottle.fillVolume();
+        assertEquals(100, waterBottle.getVolume());
+    }
 }
